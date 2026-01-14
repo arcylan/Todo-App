43e386e38410ad183c97c8153df71f56ad2abe0e
arcylan <arcylanlone@gmail.com>
2026-01-14 10:45:42 +0530

    Initial commit

---
+++ src/main/java/com/app/todoapp/service/TaskService.java
@@ -0,0 +1,38 @@
+package com.app.todoapp.service;
+
+import com.app.todoapp.models.Task;
+import com.app.todoapp.repositories.TaskRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class TaskService {
+    private final TaskRepository taskRepository;
+
+    public TaskService(TaskRepository taskRepository) {
+        this.taskRepository = taskRepository;
+    }
+
+    public List<Task> getAllTasks() {
+        return taskRepository.findAll();
+    }
+
+    public void createTask(String title) {
+        Task task = new Task();
+        task.setTitle(title);
+        task.setComplete(false);
+        taskRepository.save(task);
+    }
+
+    public void deleteTask(Long id) {
+        taskRepository.deleteById(id);
+    }
+
+    public void toggleTask(Long id) {
+        Task task = taskRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("Invaild Task ID"));
+        task.setComplete(!task.isComplete());
+        taskRepository.save(task);
+    }
+}
